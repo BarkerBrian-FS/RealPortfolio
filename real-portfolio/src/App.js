e50621ee36a2bb840a124dd12b995996a89b50c3
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2024-03-01 13:10:07 -0600

    added routes

--- real-portfolio/src/App.js
+++ real-portfolio/src/App.js
@@ -1,10 +1,20 @@
-
+import React, { Component } from 'react';
+import { Routes , Route } from 'react-router-dom'
+import Dashboard from "/Pages/Dashboard";
+import About from "/Pages/About";
+import Projects from "/Pages/Projects";
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    
+      <section>
+        <Routes>
+          <Route path="/dashboard" element={<Dashboard/>} />
+          <Route path="about" element={<About/>} />
+          <Route path="projects" element={<Projects/>} />
+        </Routes>
+      </section>
     </div>
   );
 }
